86b70df6617d6b557cfef286d51fd2da99724003
joaoaugusto-jesus <147534481+joaoaugusto-jesus@users.noreply.github.com>
2024-03-04 20:24:58 +0000

    fisrt commits

---
+++ src/js/component/nav.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const NavBar = () => {
+        
+    return (
+        <nav className="Nav">
+            <ul>
+                <li className="first"><a href="#">Start Bootsrap</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+
+
+
+export default NavBar;
