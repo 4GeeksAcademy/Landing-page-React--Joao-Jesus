b672e1b9a351fff0c9ebfae47a506180f38760dd
joaoaugusto-jesus <147534481+joaoaugusto-jesus@users.noreply.github.com>
2024-03-11 17:29:06 +0000

    final commits footer but media queries not working

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,9 +6,10 @@ import Footer from "./Footer"
 
 // include images into your bundle
 
-// Using the pros from jsx files to render in the index file 
+// Using the pros from jsx files to render at Home 
 const Home = () => {
     return (
+        <>
         <div className="container">
         <NavBar title='Start Bootstrap' />
         <JumboTron title='A warm Welcome!' description='lorem ipslum .....' btnLead='Call to action!' />   
@@ -18,8 +19,10 @@ const Home = () => {
         <Card title='Card title' text='Some quick example text to build on the card title and make up the bulk of the card content.' btntext='Find out more' image='https://media.istockphoto.com/id/944812540/pt/foto/mountain-landscape-ponta-delgada-island-azores.jpg?s=1024x1024&w=is&k=20&c=n0MfxHALBnYH4g5eLr6Fn4QWdqtorWvf-0P2vF279U8='/>
         <Card title='Card title' text='Some quick example text to build on the card title and make up the bulk of the card content.' btntext='Find out more' image='https://images.pexels.com/photos/33041/antelope-canyon-lower-canyon-arizona.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'/>
         </div>
-        <Footer />
+       
         </div>
+         <Footer />
+         </>
     );
 };
 
